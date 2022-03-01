f265c7fcade258226ff47cc845d54f923dac2c59
Ridgeway22 <boo22mickey@yahoo.com>
2022-02-28 19:58:06 -0600

    git commit -m "14th try"

--- src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
+++ src/main/java/org/launchcode/techjobs/mvc/controllers/SearchController.java
@@ -31,7 +31,7 @@ public class SearchController {
     public String displaySearchResults(Model model, @RequestParam String searchType, @RequestParam String searchTerm) {
         ArrayList<Job> jobs;
 
-        if (searchType.equals("all") && searchTerm.equals("")){
+        if (searchType.equals("all") || searchTerm.equals("")){
              //jobData.findByValue(searchTerm);
             jobs = JobData.findAll();
             //model.addAttribute("all", "all");
